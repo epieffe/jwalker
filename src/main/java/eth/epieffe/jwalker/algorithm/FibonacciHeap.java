6d97ae896a0a90225942c165f0492db2b0158a53
epieffe.eth <ferrariepifanio@gmail.com>
2025-06-09 23:47:38 +0200

    Update license to Apache 2.0 and add license header to each file

--- src/main/java/eth/epieffe/jwalker/algorithm/FibonacciHeap.java
+++ src/main/java/eth/epieffe/jwalker/algorithm/FibonacciHeap.java
@@ -14,6 +14,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file has been modified by Epifanio Ferrari in 2025 to adapt it for use in JWalker.
  */
 package eth.epieffe.jwalker.algorithm;
 
